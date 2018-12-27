4a84c061bb02640ab135cbdf25963c2a55626275
Angelo Alexandrou <45016403+Angelo125@users.noreply.github.com>
2018-12-27 15:26:23 +0000

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,16 @@
+# Script removes files with the same name but different extension
+# Run with Windows PowerShell
+
+# Extension to keep
+$ExtRetain = ".cso"
+
+# Path
+$Directory = "Drive:\Dir\"
+
+# Get files to keep
+$FilesToKeep = Get-ChildItem -Path $Directory -Filter *$ExtRetain
+
+# Delete duplicates with different extension
+ForEach ($File in $FilesToKeep) {
+    Get-ChildItem -Path $Directory | Where {$_.BaseName -eq $File.BaseName -and $_.Extension -ne $ExtRetain} | Remove-Item -Force
+}
